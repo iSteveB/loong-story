a0a3f323c9369bc65a85423766c8c9743996be7e
Steve Basse <84519539+iSteveB@users.noreply.github.com>
2022-08-18 19:23:20 +0200

    LogIn LogOut

--- routes/user.routes.js
+++ routes/user.routes.js
@@ -12,7 +12,7 @@ router.get('/', userController.getAllUsers);
 router.get('/:id', userController.userInfo);
 router.put('/:id', userController.updateUser);
 router.delete('/:id', userController.deleteUser);
-router.patch('/follow/:id', userController.follow)
-router.patch('/unfollow/:id', userController.unfollow)
+router.patch('/follow/:id', userController.follow);
+router.patch('/unfollow/:id', userController.unfollow);
 
 module.exports = router;
--- server.js
+++ server.js
@@ -5,11 +5,19 @@ const userRoutes = require('./routes/user.routes');
 require('dotenv').config({ path: './config/.env' });
 require('./config/db');
 
+const {checkUser, requireAuth} = require('./middlewares/auth.middleware')
+
 const app = express();
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
+
+//Middleware jwt
+app.get('*', checkUser);
+app.get('/jwtid', requireAuth, (req, res) => {
+    res.status(200).send(res.locals.user._id)
+})
 
 //Routes
 app.use('/api/user', userRoutes);
