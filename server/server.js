516749ce700e3b32aa106f61c40b84a067dc9188
Steve Basse <84519539+iSteveB@users.noreply.github.com>
2023-05-06 17:00:46 +0200

    update cors

--- server/server.js
+++ server/server.js
@@ -12,15 +12,12 @@ const { checkUser, requireAuth } = require('./middlewares/auth.middleware');
 
 const app = express();
 
-const corsOptions = {
-    origin: "https://loong-story.onrender.com/",
-    credentials: true,
-    allowedHeaders: ['sessionId', 'Content-Type'],
-    exposedHeaders: ['sessionId'],
-    methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
-    preflightContinue: false,
-};
-app.use(cors(corsOptions));
+app.use((req, res, next) => {
+    res.setHeader('Access-Control-Allow-Origin', '*');
+    res.setHeader('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content, Accept, Content-Type, Authorization');
+    res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, PATCH, OPTIONS');
+    next();
+  });
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
