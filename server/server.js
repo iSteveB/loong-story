35abc4de1994fe73ac7e1b219dd82a4a10e6cb13
Steve Basse <84519539+iSteveB@users.noreply.github.com>
2023-05-07 16:53:08 +0200

    cors options deleted

--- server/server.js
+++ server/server.js
@@ -20,7 +20,7 @@ const corsOptions = {
     methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
     preflightContinue: false,
 };
-app.use(cors(corsOptions));
+app.use(cors());
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
