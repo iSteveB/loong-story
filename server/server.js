9ba8a6caa6a1e9e4e2a9d612d9c2f51d263d6c86
Steve Basse <84519539+iSteveB@users.noreply.github.com>
2023-05-07 17:02:48 +0200

    update cors with 'authorization' in Headers

--- server/server.js
+++ server/server.js
@@ -15,7 +15,7 @@ const app = express();
 const corsOptions = {
     origin: process.env.CLIENT_URL,
     credentials: true,
-    allowedHeaders: ['sessionId', 'Content-Type'],
+    allowedHeaders: ['sessionId', 'Content-Type', 'Authorization'],
     exposedHeaders: ['sessionId'],
     methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
     preflightContinue: false,
