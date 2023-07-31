91050a33e223ba93253c334d69e8fe3c117d7936
Steve Basse <84519539+iSteveB@users.noreply.github.com>
2023-07-31 11:04:27 +0200

    Cors access update

--- server/server.js
+++ server/server.js
@@ -14,7 +14,7 @@ const app = express();
 
 // Enable CORS for all routes
 app.use((req, res, next) => {
-    res.setHeader('Access-Control-Allow-Origin', '*');
+    res.setHeader('Access-Control-Allow-Origin', 'https://loongstory.onrender.com/home');
     res.setHeader('Permissions-Policy', 'ch-ua-form-factor');
     res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, PATCH');
     res.setHeader('Access-Control-Allow-Headers', 'Content-Type, Authorization');
