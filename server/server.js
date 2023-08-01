8fd7c631685d33a6752342d2f0f5b9beddc1bf7b
Steve Basse <84519539+iSteveB@users.noreply.github.com>
2023-08-01 13:34:04 +0200

    cors

--- server/server.js
+++ server/server.js
@@ -21,15 +21,16 @@ const corsOptions = {
   preflightContinue: true,
 }
 
+app.options(cors())
 app.use(cors(corsOptions))
 
-app.use((req, res, next) => {
+/*app.use((req, res, next) => {
   res.setHeader('Access-Control-Allow-Origin', 'https://loongstory.onrender.com');
   res.setHeader('Access-Control-Allow-Credentials', 'true');
   res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, PATCH');
   res.setHeader('Access-Control-Allow-Headers', 'Content-Type, Authorization');
   next();
-});
+});*/
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
