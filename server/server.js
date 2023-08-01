02a11bcdd286f5f5edfe8e27ad88f23fc2f70d39
Steve Basse <84519539+iSteveB@users.noreply.github.com>
2023-08-01 11:38:05 +0200

    change credentials

--- server/server.js
+++ server/server.js
@@ -16,9 +16,9 @@ const app = express();
 
 app.use((req, res, next) => {
   res.setHeader('Access-Control-Allow-Origin', 'https://loongstory.onrender.com');
+  res.setHeader('Access-Control-Allow-Credentials', 'true');
   res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, PATCH');
   res.setHeader('Access-Control-Allow-Headers', 'Content-Type, Authorization');
-  res.setHeader('Access-Control-Allow-Credentials', 'true');
   next();
 });
 
