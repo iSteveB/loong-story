c1af61272047a16b1ea8590de49dd106896aeb9b
Steve Basse <84519539+iSteveB@users.noreply.github.com>
2023-08-04 17:27:01 +0200

    update cors origin

--- server/server.js
+++ server/server.js
@@ -14,7 +14,7 @@ const app = express();
 
 // Enable CORS
 const corsOptions = {
-    origin: ['https://loongstory.onrender.com', 'https://loong-story.onrender.com/api/post/', 'https://loong-story.onrender.com/jwtid', 'https://loong-story.onrender.com/api/user/'],
+    origin: req.headers.origin,
     credentials: true,
     methods: ['GET', 'POST', 'PUT', 'DELETE', 'PATCH'],
     allowedHeaders: ['Content-Type', 'Authorization'],
